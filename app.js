fc14e59ee3cd1f838feccfa397f006c960708319
Romeu Reif <romeureif10@gmail.com>
2025-01-17 20:29:24 -0300

    validar entrada de amigos e atualizar array de amigos

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let nomes = [];
+let nomeAmigos = [];
 
 function adicionarAmigo(){
-    let valorInput = document.getElementById('amigo').value;
+    let exibirNomes = document.getElementById('listaAmigos'); // pegando a ul que mostrarar os nomes
+    let valorInput = document.getElementById('amigo').value; // pegando o valor do input
     console.log(valorInput);
-    nomes.push(valorInput);
+    if (valorInput == '') {
+        alert("Por favor, insira um nome.");
+    } else {
+        nomeAmigos.push(valorInput); // adicionando dentro da lista
+        exibirNomes.innerHTML = nomeAmigos;
+        limparCampo()
+    }
+    
+    
     console.log(nomes);
 }
+
