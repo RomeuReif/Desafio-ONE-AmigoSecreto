0b4eeb0b320743ccf735473ed20de3d440bc69e4
Romeu Reif <romeureif10@gmail.com>
2025-01-17 21:05:30 -0300

    criar function que pecorre uma lista e cria elemento

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let nomeAmigos = [];
+let nomesAmigos = [];
 
 function adicionarAmigo(){
-    let exibirNomes = document.getElementById('listaAmigos'); // pegando a ul que mostrarar os nomes
     let valorInput = document.getElementById('amigo').value; // pegando o valor do input
-    console.log(valorInput);
     if (valorInput == '') {
         alert("Por favor, insira um nome.");
     } else {
-        nomeAmigos.push(valorInput); // adicionando dentro da lista
-        exibirNomes.innerHTML = nomeAmigos;
-        limparCampo()
+        nomesAmigos.push(valorInput); // adicionando dentro da lista
+        percorrerLista();
+        limparCampo();
     }
     
-    
-    console.log(nomes);
+    //console.log(nomesAmigos);
 }
 
 function limparCampo() {
     valorInput = document.querySelector('input');
     valorInput.value = '';
 };
+
+
+function percorrerLista(){
+    let exibirNomes = document.getElementById('listaAmigos');// pegando a ul que mostrarar os nomes
+    exibirNomes.innerHTML = ''; // limpa 
+    for (let i = 0;  i < nomesAmigos.length; i++){
+        let li = document.createElement('li'); // cria elemento li
+        li.innerHTML = nomesAmigos[i]; // adiciona o texto
+        exibirNomes.appendChild(li); // adiciona o li na lista ul
+        console.log(nomesAmigos[i])
+    }
+   
+}
+
